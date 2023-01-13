9fccab90bcb9772afb570fcb07e695e5415bf2c3
Mohammad Rahil <rahilms296@gmail.com>
2023-01-13 19:20:13 +0530

    Completed

---
+++ data.js
@@ -0,0 +1,7 @@
+const data = [
+    {name: "Rehil", email:"reshl@gmail.com", contact:"245353"},
+    {name: "Bansuri", email:"bans@outlook.com", contact:"35555"},
+    {name:"Deni", email:"deni@gmail.com"}
+]
+
+module.exports = data;
--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const http = require("http");
+const data = require("./data");
+
+// http
+//   .createServer((req, res) => {
+//     res.write("<h1>This is the local server created using nodejs</h1>");
+//     res.end();
+//   })
+//   .listen(4500);
 
 http
-  .createServer((req, res) => {
-    res.write("<h1>This is the local server created using nodejs</h1>");
-    res.end();
+  .createServer((req, resp) => {
+    resp.writeHead(200, { "Content-Type": "application/json" });
+    resp.write(JSON.stringify(data));
+    resp.end();
   })
-  .listen(4500);
+  .listen(5000);
